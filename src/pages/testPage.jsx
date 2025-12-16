c1ed5fe73add308e6dac24d9b7e5ce0bd274f6dd
Eranga <erangakavishanka@icloud.com>
2025-12-16 23:18:19 +0530

    example test page

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,7 +1,9 @@
 export default function TestPage(){
   return (
-    <div className = "w-full h-screen bg-red-900 flex">
-      <div className = "w-[250px] h-[250px] bg-green">
+    <div className = "w-full h-screen  flex justify-center items-center">
+      <div className = "w-[250px] h-[450px] bg-green-400">
+        <button className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] ">test</button>
+
 
 
 
