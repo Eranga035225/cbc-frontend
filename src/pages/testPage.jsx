f0d657daac6feb44ba6233f728013a9565f02ce7
Eranga <erangakavishanka@icloud.com>
2025-12-16 23:28:18 +0530

    created section

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,9 +1,13 @@
 export default function TestPage(){
+  let count = 0;
   return (
     <div className = "w-full h-screen  flex justify-center items-center">
-      <div className = "w-[250px] h-[450px] bg-green-400">
-        <button className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] ">test</button>
-
+      <div className = "w-[450px] h-[250px] shadow  flex justify-center items-center">
+        <button className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer ">-</button>
+        <span className="text-[20px] font-bold text-center w-[100px] h-[40px] mx-[10px]">
+          {count}
+        </span>
+        <button className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer">+</button>
 
 
 
