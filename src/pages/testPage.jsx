eb4bb26144cbc4ad3de8d1936132ba9313ef20f3
Eranga <erangakavishanka@icloud.com>
2025-12-17 06:30:03 +0530

    add nclick functions

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -3,11 +3,16 @@ export default function TestPage(){
   return (
     <div className = "w-full h-screen  flex justify-center items-center">
       <div className = "w-[450px] h-[250px] shadow  flex justify-center items-center">
-        <button className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer ">-</button>
-        <span className="text-[20px] font-bold text-center w-[100px] h-[40px] mx-[10px]">
+        <button onClick = {()=>{
+          console.log("clicked")
+        }}
+        className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer ">-</button>
+        <span className="text-[20px] font-bold text-center w-[100px] h-[40px] mx-[10px] flex justify-center items-center">
           {count}
         </span>
-        <button className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer">+</button>
+        <button onClick = {()=> 
+          console.log("clicked")
+        } className="bg-blue-600 text-white font-bold text-center w-[100px] h-[40px] text-[20px] cursor-pointer">+</button>
 
 
 
