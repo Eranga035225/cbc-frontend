17fd278368b54ec32a46e22121e09260d1b34428
Eranga <erangakavishanka@icloud.com>
2025-12-19 20:52:49 +0530

    feat: implement file upload functionality in TestPage

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
+import { createClient } from '@supabase/supabase-js';
+
 export default function TestPage(){
   const [image, setImage] = useState(null);
+  const url = 'https://omfainenyhrufrqtiyap.supabase.co';
+  const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tZmFpbmVueWhydWZycXRpeWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNTI0MTIsImV4cCI6MjA4MTcyODQxMn0.QW_GztE8ZXQKxN_5WMunZ41dPW16LrnQ-UnaLF_1b6Q';
+ 
 
-  
+  const supabase = createClient(url, key);
 
+  function fileUpload(){
+    supabase.storage.from('images').upload(image.name, image)
+
+    
+
+
+
+  }
 
 
   return (
@@ -18,6 +31,7 @@ export default function TestPage(){
       />
       <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition">
         Upload File
+        onClick={fileUpload}
       </button>
 
 
