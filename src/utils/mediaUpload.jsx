3bdbf61ee62a833e19ee46e3b5fe1e431026aa9d
Eranga <erangakavishanka@icloud.com>
2025-12-19 21:05:34 +0530

    feat: implement media upload utility with Supabase client setup

---
+++ src/utils/mediaUpload.jsx
@@ -0,0 +1,17 @@
+import { createClient } from '@supabase/supabase-js';
+
+
+
+const url = 'https://omfainenyhrufrqtiyap.supabase.co';
+const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tZmFpbmVueWhydWZycXRpeWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNTI0MTIsImV4cCI6MjA4MTcyODQxMn0.QW_GztE8ZXQKxN_5WMunZ41dPW16LrnQ-UnaLF_1b6Q';
+ 
+
+const supabase = createClient(url, key);
+
+
+export default function mediaUpload(file){
+    
+
+
+
+}
