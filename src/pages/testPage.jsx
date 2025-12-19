e8e39c50390e257b89d04440f42eba62deacd417
Eranga <erangakavishanka@icloud.com>
2025-12-19 21:02:30 +0530

    feat: add Supabase dependencies and update TestPage to handle file uploads

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -43,9 +43,9 @@ export default function TestPage(){
       }
     }
       />
-      <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition">
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition"  onClick={fileUpload}>
         Upload File
-        onClick={fileUpload}
+       
       </button>
 
 
