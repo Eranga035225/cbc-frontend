e677a23c30545073b3de1c07805d6bc589f6d1a4
Eranga <erangakavishanka@icloud.com>
2025-12-27 11:44:34 +0530

    function to add to acrt cart

--- src/utils/cart.jsx
+++ src/utils/cart.jsx
@@ -12,3 +12,37 @@ export function getCart(){
 
 
 }
+
+//function to add a product
+
+export function addToCart(product,quantity){
+
+  let cart = getCart();
+
+  let index = cart.findIndex((item)=> {
+    return item.productId == product.productId
+  })
+
+  if(index == -1){
+    cart[cart.length] = {
+      productId: product.productId,
+      name: product.name,
+      image: product.images[0],
+      price: product.price,
+      quantity: quantity
+
+    }
+  }else{
+    const newQuantity = cart[index].quantity + quantity;
+    if(newQuantity > 0 ){
+      //remove item from cart
+    }else{
+      cart[index].quantity = newQuantity;
+    }
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+
+
+
+}
