853aa00e8c0ff689394159d355102ac830d5c823
Eranga <erangakavishanka@icloud.com>
2025-12-29 23:27:57 +0530

    front end fucntion to handle ot send functiojn

--- src/pages/forgetPassword.jsx
+++ src/pages/forgetPassword.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
 export default function ForgetPasswordPage(){
 
   const [otpSent, setOtpSent] = useState(false);
@@ -5,10 +9,37 @@ export default function ForgetPasswordPage(){
   const [otp,setOtp] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const[confrimPassword, setConfrimPassword] = useState('');
+  function sendOtp(){
+
+    axios.post(import.meta.env.VITE_BACKEND_URI + '/api/users/send-otp',{
+      email: email
+      
+    }).then((res) => {
+      console.log(res.data);
+      setOtpSent(true);
+      toast.success('OTP sent successfully');
+    }).catch((err) => {
+      console.log(err);
+    })
+
+  }
 
   return (
 
     <div className="w-full h-screen flex justify-center items-center bg-[url('/login2.jpg')] bg-cover bg-center'">
+
+      otpSent? 
+      <div className="w-[400px] h-[400px] bg-white rounded-2xl">
+
+
+      </div>
+      : 
+      <div className="w-full h-full flex justify-center items-center bg-[url('/login2.jpg')] bg-cover bg-center'">  
+          <input type="email" placeholder="Enter your email" className=""></input>
+          <button onClick={() => setOtpSent(true)}>Send OTP</button>
+      
+      
+      </div>
 
 
 
